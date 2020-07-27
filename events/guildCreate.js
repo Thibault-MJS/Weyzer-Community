10cc1ca5bef9ffd319c6d1af4a5cf7d1d5b86c86
Thibault-MJS <68784661+Thibault-MJS@users.noreply.github.com>
2020-07-27 14:23:27 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -7,5 +7,6 @@ const { Client, Guild } = require('discord.js'); // !important
  * @param {Guild} guild 
  */
 module.exports = (bot, guild) => {
-
+    // ! Vérifier si le serveur fait partie des serveurs autorisés.
+    if (!bot.config.guilds.includes(guild.id)) return guild.leave();
 }
