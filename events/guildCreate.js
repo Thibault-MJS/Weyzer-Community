f34611838c970532f87e68efb1f8552a53b8298c
Thibault-MJS <68784661+Thibault-MJS@users.noreply.github.com>
2020-07-27 02:48:39 +0200

    Creating guildCreate event

---
+++ events/guildCreate.js
@@ -0,0 +1,11 @@
+const { Client, Guild } = require('discord.js'); // !important
+
+/**
+ * Événement guildCreate.
+ * 
+ * @param {Client} bot 
+ * @param {Guild} guild 
+ */
+module.exports = (bot, guild) => {
+
+}
