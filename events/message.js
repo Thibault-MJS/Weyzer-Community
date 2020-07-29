debd60008d2dbf175c50d292d3841f208785ccc2
Thibault-MJS <68784661+Thibault-MJS@users.noreply.github.com>
2020-07-29 15:58:10 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,5 +1,5 @@
 const { Client, Message } = require('discord.js'); // !important
-const Random = require('../Classes/Random');
+const Random = require('../Classes/Random'); // !important
 
 /**
  * Événement message
@@ -16,14 +16,27 @@ module.exports = async (bot, message) => {
     // * Partie traitement
 
     if (!bot.db.get('ignore_channel').find({ guild: message.guild.id, channel: message.channel.id }).value()) {
-        if (message.content.startsWith(bot.config.prefix)) return;
         var xpWon = Random.between(5, 10);
-        if (!bot.db.get('profil').find({ guild: message.guild.id })) return;
+        if (!bot.db.get('profil').find({ guild: message.guild.id, user: message.author.id }).value()) return bot.db.get('profil').push({ _id: Random.generateID(20),guild: message.guild.id, user: message.author.id, xp: xpWon, lvl: 0, but: 1500, money: 0, description: null, color: null, emote: null }).write();
+        var profil = Object.values(bot.db.get('profil').filter({ guild: message.guild.id, user: message.author.id }).find('user').value());
+        if ((profil[3] + xpWon) >= profil[5]) {
+            let difference = (profil[3] += xpWon) - profil[5];
+            bot.db.get('profil').find({ guild: message.guild.id, user: message.author.id }).assign({ xp: difference, but: profil[5]+=390, lvl: profil[4]+=1 }).write();
+            message.react("🆙").then(msg => {
+                let channelLvlUp = message.guild.channels.cache.find(channel => channel.id === bot.config.channels.lvl);
+                channelLvlUp.send(``, { embed: {
+                    color: bot.config.colors.success,
+                    description: `**GG ${message.author}! Vous êtes arrivé(e) niveau \`${profil[4]}\`** 🎉`
+                } });
+            });
+        } else {
+            bot.db.get('profil').find({ guild: message.guild.id, user: message.author.id }).assign({ xp: profil[3]+=xpWon }).write();
+        }
     };
 
     // * Partie commande
 
-    if (!message.content.startsWith(bot.config.prefix));
+    if (!message.content.startsWith(bot.config.prefix)) return;
     var args = message.content.slice(bot.config.prefix.length).trim().split(/ +/g);
     var cmd = args.shift().toLowerCase();
     var command;
