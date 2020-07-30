802b5d11f6cec900774accdb5bfb94cf472318f1
Thibault-MJS <68784661+Thibault-MJS@users.noreply.github.com>
2020-07-30 12:34:02 +0200

    Implements Likes system

--- events/message.js
+++ events/message.js
@@ -18,7 +18,7 @@ module.exports = async (bot, message) => {
     if (!bot.db.get('ignore_channel').find({ guild: message.guild.id, channel: message.channel.id }).value()) {
         if (!message.content.startsWith(bot.config.prefix)) {
             var xpWon = Random.between(5, 10);
-            if (!bot.db.get('profil').find({ guild: message.guild.id, user: message.author.id }).value()) return bot.db.get('profil').push({ _id: Random.generateID(20),guild: message.guild.id, user: message.author.id, xp: xpWon, lvl: 0, but: 1500, money: 0, description: null, color: null, emote: null }).write();
+            if (!bot.db.get('profil').find({ guild: message.guild.id, user: message.author.id }).value()) return bot.db.get('profil').push({ _id: Random.generateID(20), guild: message.guild.id, user: message.author.id, xp: xpWon, lvl: 0, but: 1500, money: 0, likes: 0, description: null, color: null, emote: null }).write();
             var profil = Object.values(bot.db.get('profil').filter({ guild: message.guild.id, user: message.author.id }).find('user').value());
             if ((profil[3] + xpWon) >= profil[5]) {
                 let difference = (profil[3] += xpWon) - profil[5];
